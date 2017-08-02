66a09a34ea23e24f6b01f85393e38dc2bb6d1f25
frane <khdakk@gmail.com>
2017-08-02 21:59:28 +0200

    ajout fichier

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<DisplayIhm.DisplayIhm> h = new Lazy<DisplayIhm.DisplayIhm>();
+            Console.WriteLine("IsValueCreated = {0}", h.IsValueCreated);
+            DisplayIhm.DisplayIhm test = h.Value;
+            Console.WriteLine("Length = {0}", test.hyi);
+
+          /*  foreach (string tmp in test.hyi)
+
+            {
+
+                Console.WriteLine(tmp);
+
+            }*/            
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ DisplayIhm/DisplayData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayIhm
+{
+    public class DisplayIhm
+    {
+        /* private TextBox filePath= null;*/
+         private string arrFromFile;
+
+       // private List<string> lines = null;
+
+        public DisplayIhm(){
+            //textToArray = System.IO.File.ReadAllLines("@"+ loadFile(filePath));
+            Console.WriteLine("List Generated:");
+           // lines = File.ReadLines(@"E:\Projet_Cesi\DNA\DNA-Data\test.txt").ToList();
+
+            arrFromFile = "ok ";
+
+
+
+
+
+
+
+        }
+
+
+        public string loadFile(TextBox tbFilePath)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            DialogResult dialogResult = fileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                tbFilePath.Text = fileDialog.FileName;
+            }
+
+            return tbFilePath.Text;
+        }
+
+
+
+        public string hyi
+        {
+            get
+            {
+               
+                return arrFromFile;
+            }
+            
+        }
+    }
+
+
+}
