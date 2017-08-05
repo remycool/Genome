e7b1159b3a85dc486a517f041dc15b32b2939c9f
frane <khdakk@gmail.com>
2017-08-05 23:11:14 +0200

    Création de la class EventsModel qui permet de récupérer les fichiers

---
+++ FileManagement/EventsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagement
+{
+    public delegate string MyDel(string str);
+    class EventsModel
+    {
+        event MyDel pickFileEvent;
+        public EventsModel()
+        {
+            this.pickFileEvent += new MyDel(this.getFile);
+        }
+
+        public string getFile(string fileName)
+        {
+            return "Id du ficheir" + fileName;
+        }
+
+
+
+    }
+}
