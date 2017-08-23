7aa194049fd867213966b01010aa226d3737510a
remy delmas <remy.cool@hotmail.fr>
2017-08-23 10:33:02 +0200

    factorisation methode dans business

--- Genome/Genome/GenomeBusiness.cs
+++ Genome/Genome/GenomeBusiness.cs
@@ -1,38 +1,30 @@
 ﻿using Cluster.Classes;
 using Cluster.Interfaces;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Genome.GenomeBusiness
 {
-    public class GenomeBusiness:IClusterizableBusiness
+    public class GenomeBusiness : IClusterizableBusiness
     {
 
         Operation IClusterizableBusiness.Calcul1(string chunk)
         {
             char charToCount = 'C';
-            Stopwatch sw = Stopwatch.StartNew();
             //opération
-            int count = 0;
-            for (int i = 0; i < chunk.Length; i++)
-            {
-                if (chunk[i] == charToCount)
-                    count++;
-            }
-            //On arrête le chrono
-            sw.Stop();
-
-            return new Operation { Resultat = count, TempsExecution = sw.ElapsedMilliseconds };
+            int count = chunk.Count(c => c == charToCount);
+            return new Operation { Resultat = count, TempsExecution = 0 };
         }
 
         Operation IClusterizableBusiness.Calcul2(string chunck)
         {
-            string brin1=string.Empty;
-            string brin2=string.Empty;
-            char [] arrayChar = chunck.ToCharArray();
+            string brin1 = string.Empty;
+            string brin2 = string.Empty;
+            char[] arrayChar = chunck.ToCharArray();
             int i = 0;
             bool end = false;
             Stopwatch sw = Stopwatch.StartNew();
-            while (!end )
+            while (!end)
             {
                 bool carriageReturn = false;
                 bool switchBrin = false;
@@ -41,7 +33,7 @@ namespace Genome.GenomeBusiness
                 while (!carriageReturn)
                 {
                     //Teste si on arrive à la fin du fichier
-                    if(i == chunck.Length)
+                    if (i == chunck.Length)
                     {
                         end = true;
                         break;
@@ -53,14 +45,14 @@ namespace Genome.GenomeBusiness
                         if (!switchBrin)
                         {
                             brin1 += arrayChar[i];
-                           switchBrin = !switchBrin;
+                            switchBrin = !switchBrin;
                         }
                         else
                             brin2 += arrayChar[i];
                     }
                     else
                         if (arrayChar[i] == '\n')
-                        carriageReturn=true;
+                        carriageReturn = true;
                     i++;
                 }
             }
